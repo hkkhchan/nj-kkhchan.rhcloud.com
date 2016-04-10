a8ac85a0213e7e339a80a5c03232333eeb3a0b64
Win10 <mrkhchan@outlook.com>
2016-04-10 17:12:48 +0800

    test

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/', function(req, res){
     res.send('hello! express！ this is a index');
 });
 
-app.listen(80, function () {
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, function () {
     console.log('ready on port 8080');
 })
