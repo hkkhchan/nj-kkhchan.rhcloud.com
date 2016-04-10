27efbc7d281bd79fb6d5200e9d90f82102f35926
Win10 <mrkhchan@outlook.com>
2016-04-10 17:16:29 +0800

    test

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var express = require('express');
 
 var app = express();
 
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 // define routes
 app.get('/', function(req, res){
     res.send('hello! express！ this is a index');
