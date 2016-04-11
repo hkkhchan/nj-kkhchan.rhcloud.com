89562bb69eb32314c9f6b18d10aa812548b921e2
Win10 <mrkhchan@outlook.com>
2016-04-11 22:45:07 +0800

    test

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ var http = require('http');
 var express = require('express');
 var fs      = require('fs');
 var app = express();
+app.set('view engine', 'ejs');
 var server = http.createServer(app);
 app.use('/css',express.static(path.join(__dirname + '/css')));
 
 app.get('/',function(req,res){
-	res.setHeader('Content-Type', 'text/html');
-    if (req.query.a != null) {
-		res.send('Hi, '+req.query.a)
-	}
-	else
-	{
-		res.send('Hi, Ken');
-	}
+	res.render('default',{
+		title: 'New Title',
+		users: ['Ken', 'Chan']
+	});
+
+app.get('/who/:name?', function(req,res) {
+	var name= req.params.name;
+	res.send(name + 'is here.');
 });
+
+app.get('*',function(req,res) {
+	res.send('nothing here');
+});
+	
+    
 server.listen(8080, process.env.OPENSHIFT_NODEJS_IP);
