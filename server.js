ebc4d0bf285a5d5be6b26466c32462d33ee4d723
Win10 <mrkhchan@outlook.com>
2016-04-11 22:56:09 +0800

    test

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ var http = require('http');
 var express = require('express');
 var fs      = require('fs');
 var app = express();
-var server = http.createServer(app);
-app.set('view engine', 'ejs');
 
+app.set('view engine', 'ejs');
+app.set('port',process.env.OPENSHIFT_NODEJS_PORT);
+app.set('ip',process.env.OPENSHIFT_NODEJS_IP);
 app.use('/css',express.static(path.join(__dirname + '/css')));
+
+var server = http.createServer(app);
 
 app.get('/',function(req,res){
 	res.render('default',{
