6b1403f163f5c2c999477ba7ffa3b84429a19f39
hkkhchan <mrkhchan@outlook.com>
2016-09-28 01:22:56 +0800

    test

--- models/db.js
+++ models/db.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.8.6.130/mydb');
+var options={
+    user: 'ken',
+    pass: 'ken'
+}
+mongoose.connect('mongodb://127.8.6.130/mydb',options);
 
 var UserSchema = mongoose.Schema({
     name: String,
