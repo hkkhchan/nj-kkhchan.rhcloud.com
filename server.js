f83d93dabf6ad96fdbc13e8cb97e8fba19efeea9
hkkhchan <mrkhchan@outlook.com>
2016-09-28 00:30:27 +0800

    test

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/who/:name?', function(req,res) {
     var name= req.params.name;
     User.findOne({'name':name},'_id password email',function(err,who){
         if (err){
-            res.send(name + ' is not registered.');
+            res.send(err);
         }
         else{
             res.send(name + 's email is ' + who.email );
