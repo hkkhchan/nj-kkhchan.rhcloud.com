7efd897ba8330cde1d16fbee3cfe117194bd353c
hkkhchan <hkkhchan@gmail.com>
2016-09-28 21:21:52 +0800

    test

--- models/db.js
+++ models/db.js
@@ -3,9 +3,8 @@ var conn='mongodb://';
 conn+=process.env.OPENSHIFT_MONGODB_DB_USERNAME+':';
 conn+=process.env.OPENSHIFT_MONGODB_DB_PASSWORD+'@';
 conn+=process.env.OPENSHIFT_MONGODB_DB_HOST + ':';
-//conn+=process.env.OPENSHIFT_MONGODB_DB_PORT + '/mydb';
-conn+='localhost/mydb';
-mongoose.connect('127.8.6.130',{config:{autoIndex: false}});
+conn+=process.env.OPENSHIFT_MONGODB_DB_PORT + '/mydb';
+mongoose.connect(conn);
 
 var UserSchema = mongoose.Schema({
     name: String,
