9989501deb6632e5d44c4618b640288dba593c6c
hkkhchan <hkkhchan@gmail.com>
2016-09-28 22:04:44 +0800

    test

--- models/db.js
+++ models/db.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose');
 var url='mongodb://';
-url+=process.env.OPENSHIFT_MONGODB_DB_USERNAME+':';
-url+=process.env.OPENSHIFT_MONGODB_DB_PASSWORD+'@';
+//url+=process.env.OPENSHIFT_MONGODB_DB_USERNAME+':';
+//url+=process.env.OPENSHIFT_MONGODB_DB_PASSWORD+'@';
 url+=process.env.OPENSHIFT_MONGODB_DB_HOST + ':';
 url+=process.env.OPENSHIFT_MONGODB_DB_PORT + '/mydb';
-//url='127.0.0.1:27017/mydb';
 mongoose.connect(url);
 
 var UserSchema = mongoose.Schema({
