c6d385e7c198527e28704a6af43eec9fc1904ccd
hkkhchan <hkkhchan@gmail.com>
2016-09-28 21:54:02 +0800

    test

--- models/db.js
+++ models/db.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
-var conn='mongodb://';
-conn+=process.env.OPENSHIFT_MONGODB_DB_USERNAME+':';
-conn+=process.env.OPENSHIFT_MONGODB_DB_PASSWORD+'@';
-conn+=process.env.OPENSHIFT_MONGODB_DB_HOST + ':';
-conn+=process.env.OPENSHIFT_MONGODB_DB_PORT + '/mydb';
-mongoose.connect(conn);
+var url='mongodb://';
+//url+=process.env.OPENSHIFT_MONGODB_DB_USERNAME+':';
+//url+=process.env.OPENSHIFT_MONGODB_DB_PASSWORD+'@';
+//url+=process.env.OPENSHIFT_MONGODB_DB_HOST + ':';
+//url+=process.env.OPENSHIFT_MONGODB_DB_PORT + '/mydb';
+url='127.0.0.1:27017/mydb';
+mongoose.connect(url);
 
 var UserSchema = mongoose.Schema({
     name: String,
