fab667b7f8ff42caee54100784edb6f1c7be3f96
hkkhchan <hkkhchan@gmail.com>
2016-09-28 22:18:11 +0800

    test

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ var url='mongodb://';
 //url+=process.env.OPENSHIFT_MONGODB_DB_USERNAME+':';
 //url+=process.env.OPENSHIFT_MONGODB_DB_PASSWORD+'@';
 url+=process.env.OPENSHIFT_MONGODB_DB_HOST + ':';
-url+=process.env.OPENSHIFT_MONGODB_DB_PORT + '/mydb';
+url+=process.env.OPENSHIFT_MONGODB_DB_PORT + '/nj';
 mongoose.connect(url);
 
 var UsersSchema = mongoose.Schema({
